b63b9a3bdd8504e8bb966d6bedec44ad3a90599c
finn-francis <finnfrancis123@gmail.com>
2018-10-21 15:22:14 +0100

    Added a spec for required fields in the MadeToMeasure::Pageview initializer
    Why?
    - Acts as documentation for the intended behaviour of the class.
    - Ensures that no-one will accidentally remove the validation in the future and accidentally break the gem.

--- spec/pageview_spec.rb
+++ spec/pageview_spec.rb
@@ -3,10 +3,9 @@
 require 'spec_helper'
 
 RSpec.describe MadeToMeasure::Pageview do
-  let(:pageview) do
-    MadeToMeasure::Pageview.new(
-      page: '/test', title: 'test', document_hostname: 'mytest.com', client_id: '111'
-    )
+  let(:pageview) { MadeToMeasure::Pageview.new(*pageview_params) }
+  let(:pageview_params) do
+    [page: '/test', title: 'test', document_hostname: 'mytest.com', client_id: '111']
   end
 
   before do
@@ -21,11 +20,22 @@ RSpec.describe MadeToMeasure::Pageview do
       @request = stub_request(:post, 'https://www.google-analytics.com/collect?cid=111&dh=mytest.com&dp=/test&dt=test&t=pageview&tid=UA-12345-6&v=1')
                  .with(headers: { 'User-Agent': 'MadeToMeasure/0.2.2 (+https://github.com/dmgarland/made-to-measure)' })
                  .to_return(status: 200, body: '', headers: {})
-      pageview.commit
     end
 
-    it 'should send a page tracker request to google' do
-      expect(@request).to have_been_requested
+    context 'including all required parameters' do
+      before { pageview.commit }
+
+      it 'should send a page tracker request to google' do
+        expect(@request).to have_been_requested
+      end
+    end
+
+    context 'missing required parameters' do
+      let(:pageview_params) { [title: 'test', document_hostname: 'mytest.com', client_id: '111'] }
+
+      it 'should raise an argument' do
+        expect { pageview.commit }.to raise_error(ArgumentError).with_message('missing keyword: page')
+      end
     end
   end
 
